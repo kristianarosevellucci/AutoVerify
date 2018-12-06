88f81bd6772ee139c73887f63c10e7c653a7f70c
Kristiana Vellucci <kristianavellucci@Kristianas-MacBook-Pro-3.local>
2018-12-06 02:11:59 -0500

    styling structure setup

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <router-view />
     <Footer
-      poweredBy="Powered by Mobials"
+      poweredBy="Powered by "
       footerCenterText="192 Dundas St Suite 300 | London, ON N6A 1G7 | Tel: 1.877.833.1423"
       copyRight="Copyright 2019. All rights reserved."
     />
@@ -24,4 +24,5 @@ export default {
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
+@import url('https://fonts.googleapis.com/css?family=Cairo:400,600,700|Montserrat:400,700,800');
 </style>
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,8 +1,8 @@
 <template>
-    <footer class="container-fluid py-2">
+    <footer class="container-fluid py-4">
       <div class="row">
         <div class="col-sm-3">
-          <p>{{poweredBy}}</p>
+          <p>{{poweredBy}}<a href="www.mobials.com" target="blank">Mobials</a></p>
         </div>
         <div class="col-sm-6 text-center">
           <p>{{footerCenterText}}</p>
@@ -35,11 +35,5 @@
 </script>
 
 <style lang="scss" scoped>
-  $red: #ff0000;
-
-  p {
-    font-family: 'Cairo', sans-serif;
-    font-size: 14px;
-    color: $red;
-  }
+  @import "../../public/scss/application.scss";
 </style>
